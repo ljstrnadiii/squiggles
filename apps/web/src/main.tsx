fb42fa475135b0b69d5f080b8a2bf5aca5c68a83
Len Strnad <ljstrnadiii@gmail.com>
2026-09-08 09:53:52 -0600

    experiment: route terrain flag to real-data MapLibre fork app

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { PanelEnhancements } from "./PanelEnhancements";
+import { TerrainExperimentApp } from "./TerrainExperimentApp";
 import "./styles.css";
 import "./panelEnhancements.css";
 import "./spatial.css";
 import "./mapDisclosure.css";
 
-createRoot(document.getElementById("root")!).render(<StrictMode><><App /><PanelEnhancements /></></StrictMode>);
+const terrainExperiment = new URLSearchParams(window.location.search).get("terrain") === "1";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    {terrainExperiment ? <TerrainExperimentApp /> : <><App /><PanelEnhancements /></>}
+  </StrictMode>,
+);
