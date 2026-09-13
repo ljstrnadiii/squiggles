a1ffc0a6316edc1da1e793cea961d1db378e9399
Len Strnad <ljstrnadiii@gmail.com>
2026-09-12 19:12:39 -0600

    feat: mount map navigation enhancements

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./maplibreWorker";
 import { App } from "./App";
+import { MapNavigationEnhancements } from "./MapNavigationEnhancements";
 import { PanelEnhancements } from "./PanelEnhancements";
 import { initErrorTelemetry } from "./errorTelemetry";
 import "./styles.css";
 import "./panelEnhancements.css";
+import "./mapNavigationEnhancements.css";
 import "./spatial.css";
 import "./mapDisclosure.css";
 
 initErrorTelemetry();
-createRoot(document.getElementById("root")!).render(<StrictMode><><App /><PanelEnhancements /></></StrictMode>);
+createRoot(document.getElementById("root")!).render(<StrictMode><><App /><PanelEnhancements /><MapNavigationEnhancements /></></StrictMode>);
