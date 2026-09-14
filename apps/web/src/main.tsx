45fe75a7b5cfb93679396ae52c73e117c33a36e0
Len Strnad <ljstrnadiii@gmail.com>
2026-09-13 20:35:03 -0600

    fix(web): apply redesigned navigation before first paint

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -7,15 +7,17 @@ import { PanelEnhancements } from "./PanelEnhancements";
 import { initDuckDBProgressIndicator } from "./duckdbProgressIndicator";
 import { initErrorTelemetry } from "./errorTelemetry";
 import "./styles.css";
+import "./designTokens.css";
 import "./panelEnhancements.css";
 import "./mapNavigationEnhancements.css";
-import "./responsivePaneFixes.css";
+import "./responsivePanels.css";
 import "./spatial.css";
 
 if (/^\/m\/[0-9a-f-]{36}\/?$/i.test(window.location.pathname)) {
   window.history.replaceState({}, "", "/");
 }
 
+document.documentElement.classList.add("map-navigation-redesign");
 initErrorTelemetry();
 initDuckDBProgressIndicator();
 createRoot(document.getElementById("root")!).render(<StrictMode><><App /><PanelEnhancements /><MapNavigationEnhancements /></></StrictMode>);
